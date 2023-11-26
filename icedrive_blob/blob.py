1ce876cbd39b3ad20a4fd33cd4adf3e84a306004
Sergio <Sergio.Cornejo@alu.uclm.es>
2023-11-26 13:41:32 +0100

    Funciones init, read y close

--- icedrive_blob/blob.py
+++ icedrive_blob/blob.py
@@ -1,17 +1,49 @@
 """Module for servants implementations."""
 
 import Ice
-
 import IceDrive
+import hashlib
+import os
 
 class DataTransfer(IceDrive.DataTransfer):
     """Implementation of an IceDrive.DataTransfer interface."""
 
+    def __init__(self, file_path: str, ) -> IceDrive.DataTransfer:
+        """Initialize the DataTransfer object."""
+        self.file_path = file_path
+
+        with open(file_path, "rb") as f:
+            self.file_content = f.read()
+        
+        self.sumSHA256 = hashlib.sha256(self.file_content).hexdigest()
+        
+        with open('blobs.txt', 'w') as f:
+            f.write(self.sumSHA256)
+            f.write('\n')
+
     def read(self, size: int, current: Ice.Current = None) -> bytes:
         """Returns a list of bytes from the opened file."""
+        content_list = []
+
+        with open(self.file_path, "rb") as f:
+            chunck = f.read(size)
+            if not chunck:
+                return None
+            content_list.append(chunck)
+            return content_list
 
     def close(self, current: Ice.Current = None) -> None:
         """Close the currently opened file."""
+        self = None
+
+    @staticmethod
+    def is_hash_present(file_path: str, target_hash: str) -> bool:
+        """Check if a hash is already present in a file."""
+        if not os.path.exists(file_path):
+            return False
+
+        with open(file_path, 'r') as f:
+            return any(line.strip() == target_hash for line in f)
 
 class BlobService(IceDrive.BlobService):
     """Implementation of an IceDrive.BlobService interface."""
