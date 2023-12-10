c2b2c28d021e615afa553ca723ecffb481b2854c
Sergio <Sergio.Cornejo@alu.uclm.es>
2023-12-10 07:35:43 +0100

    Cambios en appPruebas

--- icedrive_blob/appPruebas.py
+++ icedrive_blob/appPruebas.py
@@ -10,24 +10,6 @@ import IceDrive
 from .blob import BlobService 
 from .blob import DataTransfer
 
-#Clase con la que ejecutamos el servicio de Blob
-class BlobApp(Ice.Application):
-    def run(self, args: List[str]) -> int:
-        adapter = self.communicator().createObjectAdapter("BlobAdapter")
-        adapter.activate()
-
-        path_archivos = "/home/sergio/Escritorio/VSCodeLinux/LAB-SSDD-23-24/icedrive_blob/archivos.txt"
-
-        servant = BlobService(path_archivos)
-        servant_proxy = adapter.addWithUUID(servant)
-
-        logging.info("Proxy sirviente BlobService: %s", servant_proxy)
-
-        self.shutdownOnInterrupt()
-        self.communicator().waitForShutdown()
-
-        return 0
-
 #Clase con la que he estado ejecutando pruebas para verificar el funcionamiento de los metodos
 class BlobAppPruebas(Ice.Application):
     """Implementation of the Ice.Application for the Authentication service."""
