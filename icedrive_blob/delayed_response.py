4655eb3138232c53c4500a1168acdc16afff22e4
SergiooCoorne <Sergio.Cornejo@alu.uclm.es>
2023-12-26 08:08:06 +0100

    Cambios en archivo delayed_response

--- icedrive_blob/delayed_response.py
+++ icedrive_blob/delayed_response.py
@@ -4,25 +4,52 @@ import Ice
 
 import IceDrive
 
+from blob import BlobService
 
 class BlobQueryResponse(IceDrive.BlobQueryResponse):
     """Query response receiver."""
+    def __init__(self, future: Ice.Future) -> None:
+        self.future = future
+
     def downloadBlob(self, blob: IceDrive.DataTransferPrx, current: Ice.Current = None) -> None:
         """Receive a `DataTransfer` when other service instance knows the `blob_id`."""
+        self.future.set(blob) #Establecemos el proxy del DataTranfer en el objeto future
 
     def blobLinked(self, current: Ice.Current = None) -> None:
         """Indicate that `blob_id` was recognised by other service instance and was linked."""
+        print("Blob_id reconocido y enlazado\n")
 
     def blobUnlinked(self, current: Ice.Current = None) -> None:
         """Indicate that `blob_id` was recognised by other service instance and was unlinked."""
+        print("Blob_id reconocido y desenlazado\n")
 
 class BlobQuery(IceDrive.BlobQuery):
     """Query receiver."""
+    def __init__(self, blob_service: BlobService) -> None:
+        """" Receive a BlobServicePrx. This proxy will be used to download, link, or unlink the file"""
+        self.blob_service = blob_service
+
     def downloadBlob(self, blob_id: str, response: IceDrive.BlobQueryResponsePrx, current: Ice.Current = None) -> None:
         """Receive a query for downloading an archive based on `blob_id`."""
+        try:
+            data_transfer_prx = self.blob_service.donwload(blob_id)
+            response.downloadBlob(data_transfer_prx)
+        except IceDrive.UnknownBlob:
+            print("Blob_id no encontrado\n")
 
     def linkBlob(self, blob_id: str, response: IceDrive.BlobQueryResponsePrx, current: Ice.Current = None) -> None:
         """Receive a query to create a link for `blob_id` archive if it exists."""
+        try:
+            self.blob_service.link(blob_id)
+            response.blobLinked()
+        except IceDrive.UnknownBlob:
+            print("Blob_id no encontrado\n")
+
 
     def unlinkBlob(self, blob_id: str, response: IceDrive.BlobQueryResponsePrx, current: Ice.Current = None) -> None:
         """Receive a query to destroy a link for `blob_id` archive if it exists."""
+        try:
+            self.blob_service.unlink(blob_id)
+            response.blobUnlinked()
+        except IceDrive.UnknownBlob:
+            print("Blod_id no encontrado\n")
